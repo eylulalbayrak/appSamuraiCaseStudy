c7ed45d7a39704269b2c303d0d50be089525b28b
eylulalbayrak <eylul.albayrak@metu.edu.tr>
2022-05-19 19:08:42 +0300

    Read and Convert URL

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from urllib.request import urlopen
+import json
+
+
+def getJSON(url):
+    source = urlopen(url)
+
+    dataJSON = json.loads(source.read())
+
+    return dataJSON
+
+
 if __name__ == '__main__':
 
     url = "https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_2.json"
+
+    data = getJSON(url)
