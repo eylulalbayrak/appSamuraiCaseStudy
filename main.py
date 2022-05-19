e81024604a51f095d209401f00baaf0c08fb1298
eylulalbayrak <eylul.albayrak@metu.edu.tr>
2022-05-19 19:14:42 +0300

    Calculate Price

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from urllib.request import urlopen
 import json
 
 
+def calculatePrice(data):
+
+    if 'items' not in data:
+        return data['count'] * data['price']
+
+    elif not isinstance(data['items'], list):
+        return data['count'] * calculatePrice(data['items'])
+
+    else:
+        sum = 0
+
+        for key in data['items']:
+            sum += calculatePrice(key)
+
+        return data['count'] * sum
+
+
 def getJSON(url):
     source = urlopen(url)
 
@@ -15,3 +32,5 @@ if __name__ == '__main__':
     url = "https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_2.json"
 
     data = getJSON(url)
+
+    print(f"The result is: {calculatePrice(data)}")
